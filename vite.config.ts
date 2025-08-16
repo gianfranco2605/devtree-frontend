df4b8f79110664f50bd17014955f3d4b23deffff
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2025-08-16 23:16:13 +0200

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   package.json
    	new file:   public/_redirects
    	new file:   src/components/nav/AdminNavigation 2.tsx
    	new file:   src/components/nav/HomeNavigation 2.tsx
    	modified:   tsconfig.node.json
    	modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      react: path.resolve(__dirname, 'node_modules/react'),
-      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
-    },
-  },
   build: {
     rollupOptions: {
       external: [
